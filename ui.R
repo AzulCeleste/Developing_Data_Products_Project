d0613ecdf44a2077c86b522f4cde18d74218d998
AzulCeleste <carlaquezada@gmail.com>
2015-11-21 22:32:49 -0600

    updates to the output description

--- ui.R
+++ ui.R
@@ -5,7 +5,6 @@ shinyUI(
      #Application Title
         headerPanel("Dog Age Calculator"),
         sidebarPanel(
-                #numericInput('humanAge', 'Age in Human Years', 1 , min=0.5, max = 14, step =5),
                 selectInput('humanAge', 'Age in Human Years', choices = ageOptions),
                 radioButtons('dogSize', "Choose the size of your dog", choices = sizes),
                 submitButton('Submit')
@@ -14,7 +13,7 @@ shinyUI(
                 h3('If your dog was a human, how old would he be?'),
                 h4('Age in Human Years (you entered)'),
                 verbatimTextOutput("inputValue"),
-                h4('Which resulted in a Age of'), 
+                h4('Which resulted in a Dog Age of'), 
                 verbatimTextOutput("dogAge"),
                 h4('and he is considered a'),
                 verbatimTextOutput("seniority")
